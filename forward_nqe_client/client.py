df0c55725a0bf8be9de45420450e23a248afcc52
Nikhil Handigol <nikhilhandigol@forwardnetworks.com>
2019-08-07 01:18:58 -0700

    Add utility functions to iterate over pages

--- forward_nqe_client/client.py
+++ forward_nqe_client/client.py
@@ -1,6 +1,7 @@
 import urllib3
 import requests
 import json
+import re
 
 
 def runQueryInternal(snapshotId, q, baseUrl, auth, verify):
@@ -26,6 +27,36 @@ def formatIpAddr(a):
     return addr + '/' + str(prefix)
 
 
+def add_cursor(query, pathToPage, cursor):
+    wordPattern = re.compile(r'(\w+?)[\(\{]*')
+    curIndex = 0
+    for match in wordPattern.findIter(query):
+        print match.group(1)
+        if match == pathToPage[curIndex]:
+            curIndex += 1
+            if curIndex == len(pathToPage):
+                print "Found match at position: " + match.end(0)
+                return query[: match.end(0)] + '(from: {})'.format() + query[match.end(0):]
+
+    raise Exception('Could not find path the path in the query: {}'.format(query))
+
+
+def get_page(dataset, pathToPage):
+    page = dataset
+    for nextField in pathToPage:
+        if not (isinstance(page, list) or isinstance(page, dict)):
+            return None
+        if nextField not in page:
+            return None
+        page = page[nextField]
+        if isinstance(page, list):
+            page = page[0]
+    assert 'pageInfo' in page, "Missing the field 'pageInfo' in:\n" + page
+    pageInfo = page['pageInfo']
+    assert 'hasNextPage' in pageInfo and 'endCursor' in pageInfo, str(pageInfo)
+    return page
+
+
 class FwdApi:
     def __init__(self, baseUrl, cred, verify=True):
         self.baseUrl = baseUrl
@@ -41,6 +72,23 @@ class FwdApi:
         result = runQueryWithVars(snapshotId, query, varsDict, self.baseUrl,
                                   self.cred, self.verify)
         return result.json()['data']
+
+    def iterate_pages(self, snapshotId, query, queryParams, pathToPage):
+        result = self.queryWithVars(snapshotId, query, queryParams)
+        page = get_page(result, pathToPage)
+        if page is None:
+            return
+        for item in page['items']:
+            yield item
+        pageInfo = page['pageInfo']
+        while pageInfo['hasNextPage']:
+            nextPageQuery = add_cursor(query, pathToPage, pageInfo['endCursor'])
+            result = self.queryWithVars(snapshotId, nextPageQuery, queryParams)
+            page = get_page(result)
+            if page is None:
+                return
+            for item in page['items']:
+                yield item
 
 
 def printTable(header, rows):
